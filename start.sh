31fc0a0664a4ee0400e477d357eed0a33caa05a8
Oleg Putsillo <o.putsillo@12.digital>
2024-12-28 18:09:00 +0300

    change arch

--- start.sh
+++ start.sh
@@ -1,9 +1,4 @@
 cd /tmp
-#Download section
-curl -O https://raw.githubusercontent.com/itdoginfo/allow-domains/main/Russia/inside-kvas.lst
-curl -O https://raw.githubusercontent.com/opusmode/kvs-script/refs/heads/main/custom.lst
-#Import section
-kvas import inside-kvas.lst
-kvas import custom.lst
-#Clear section
-rm -f *.lst
+curl -O https://raw.githubusercontent.com/opusmode/kvs-script/refs/heads/main/action.sh
+./action.sh
+rm action.sh
