c08b72a089142d8f374e639e7c57b5ad7116c9ab
Oleg Putsillo <o.putsillo@12.digital>
2024-12-28 19:28:13 +0300

    add lists

--- action.sh
+++ action.sh
@@ -1,9 +1,16 @@
-cd /tmp
+mkdir -p /tmp/import
+cd /tmp/import
 #Download section
 curl -O https://raw.githubusercontent.com/itdoginfo/allow-domains/main/Russia/inside-kvas.lst
 curl -O https://raw.githubusercontent.com/opusmode/kvs-script/refs/heads/main/custom.lst
+curl -O https://raw.githubusercontent.com/1andrevich/Re-filter-lists/refs/heads/main/community.lst
+curl -O https://raw.githubusercontent.com/1andrevich/Re-filter-lists/refs/heads/main/ooni_domains.lst
+curl -O https://raw.githubusercontent.com/1andrevich/Re-filter-lists/refs/heads/main/domains_all.lst
+#Merge section
+cat *.lst > total.lst
+cat *.txt > total.lst
 #Import section
-kvas import inside-kvas.lst
-kvas import custom.lst
+kvas import total.lst
 #Clear section
-rm -f *.lst
+cd /tmp
+rm -rf import
